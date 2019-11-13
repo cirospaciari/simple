010b535854986ea0eaa5b4d6fdb54a314e0b1a8e
Ciro <ciro@wecontent.com.br>
2019-11-13 10:10:40 -0200

    fix

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const simple = require("./simple/Simple");
 
-let tree = simple.parse("./tests/objects.simple");
+let tree = simple.parse("./tests/factorial.simple");
 const exitCode = simple.execute(tree) || 0;
 console.log("Program exited with code", exitCode);
